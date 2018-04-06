2b4483043be51d02c6e59c7c897c9326f1efdc3c
Alan Oakden <admin@alanoakden.com>
2018-04-06 01:20:28 +0100

    contact page made mobile. Start of slider in place

--- app/assets/scripts/contactHover.js
+++ app/assets/scripts/contactHover.js
@@ -1,30 +1,34 @@
 $("document").ready(function(){
-        $("#contact__contact-us").click(function(){
+        $("#contact__contact-us").click(function(e){
           $('.contact__img-one').css("background-image", "url('/assets/images/contactUs.jpg')");
             $("#contact__form-container").removeClass("display-none");
             $("#contact__phone-container").addClass("display-none");
             $("#contact__social-container").addClass("display-none");
+            e.preventDefault();
         });
 
-        $("#contact__call-us").click(function(){
+        $("#contact__call-us").click(function(e){
           $('.contact__img-one').css("background-image", "url('/assets/images/contactUs.jpg')");
             $("#contact__phone-container").removeClass("display-none");
             $("#contact__form-container").addClass("display-none");
             $("#contact__social-container").addClass("display-none");
+            e.preventDefault();
         });
 
-        $("#contact__find-us").click(function(){
+        $("#contact__find-us").click(function(e){
             $('.contact__img-one').css("background-image", "url('/assets/images/dunston-house.png')");
             $("#contact__form-container").addClass("display-none");
             $("#contact__phone-container").addClass("display-none");
             $("#contact__social-container").addClass("display-none");
+            e.preventDefault();
         });
 
-        $("#contact__social-media").click(function(){
+        $("#contact__social-media").click(function(e){
           $('.contact__img-one').css("background-image", "url('/assets/images/contactUs.jpg')");
             $("#contact__social-container").removeClass("display-none");
             $("#contact__form-container").addClass("display-none");
             $("#contact__phone-container").addClass("display-none");
+            e.preventDefault();
         });
 
 
--- app/assets/scripts/navMenu.js
+++ app/assets/scripts/navMenu.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "30vw";
+  //  document.getElementById("mySidenav").style.width = "30vw";
     $(".menu-overlay").removeClass("display-none");
     $(".menuButton").css("right", "29%");
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  //  document.getElementById("mySidenav").style.width = "0";
     $(".menu-overlay").addClass("display-none");
     $(".menuButton").css("right", "7%");
 }
